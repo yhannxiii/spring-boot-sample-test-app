856cb7818aa22959cbd85535707fe34eedfe7567
soupramanien <soupramani@yahoo.fr>
2022-05-07 13:45:44 +0200

    initial commit

---
+++ src/test/java/com/example/testingweb/smoke/SmokeTest.java
@@ -0,0 +1,22 @@
+package com.example.testingweb.smoke;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.testingweb.HomeController;
+
+@SpringBootTest
+public class SmokeTest {
+
+	@Autowired
+	private HomeController controller;
+
+	@Test
+	public void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
+	}
+}
